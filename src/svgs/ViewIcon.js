8f8a1c865c8b21fcf3476c18ab91e8bd66260fef
devseyipeters <seyi@usecrafter.com>
2024-05-22 17:55:59 +0100

    code icon works, download works, options show... but not option item

---
+++ src/svgs/ViewIcon.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function ViewIcon() {
+	return (
+		<svg
+			width="18"
+			height="18"
+			viewBox="0 0 18 18"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg">
+			<path
+				d="M11.25 9C11.25 9.59674 11.0129 10.169 10.591 10.591C10.169 11.0129 9.59674 11.25 9 11.25C8.40326 11.25 7.83097 11.0129 7.40901 10.591C6.98705 10.169 6.75 9.59674 6.75 9C6.75 8.40326 6.98705 7.83097 7.40901 7.40901C7.83097 6.98705 8.40326 6.75 9 6.75C9.59674 6.75 10.169 6.98705 10.591 7.40901C11.0129 7.83097 11.25 8.40326 11.25 9Z"
+				stroke="currentColor"
+				stroke-width="1.5"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+			/>
+			<path
+				d="M1.5 9C2.7 5.92725 5.502 3.75 9 3.75C12.498 3.75 15.3 5.92725 16.5 9C15.3 12.0728 12.498 14.25 9 14.25C5.502 14.25 2.7 12.0728 1.5 9Z"
+				stroke="currentColor"
+				stroke-width="1.5"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+			/>
+		</svg>
+	);
+}
---
+++ src/svgs/XIcon.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function XIcon() {
+	return (
+		<svg
+			width="18"
+			height="18"
+			viewBox="0 0 18 18"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg">
+			<path
+				d="M13.5 3L9.75 8.00025M9.75 8.00025L6.225 3.3C6.15514 3.20685 6.06455 3.13125 5.96041 3.07918C5.85627 3.02711 5.74143 3 5.625 3H3L8.25 9.99975M9.75 8.00025L15 15H12.375C12.2586 15 12.1437 14.9729 12.0396 14.9208C11.9354 14.8687 11.8449 14.7931 11.775 14.7L8.25 9.99975M4.5 15L8.25 9.99975"
+				stroke="black"
+				stroke-width="1.5"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+			/>
+		</svg>
+	);
+}
